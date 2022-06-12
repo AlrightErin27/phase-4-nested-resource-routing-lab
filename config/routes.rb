85d955371e5bcf571f9df3c3ab84ab7ebd488f5a
Erin Van Brunt <erinmontybruce@gmail.com>
2022-06-11 18:17:31 -0700

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :items, only: %i[show index create]
+  end
+
+  resources :users, only: %i[show index create]
+  resources :items, only: %i[show index create]
+
+  # Verb  URI Pattern                                    Controller#Action
+  # user_items GET   /users/:user_id/items(.:format)     items#index
+  # user_item GET    /users/:user_id/items/:id(.:format) items#show
+  # user GET         /users/:id(.:format)                users#show
+  # items GET        /items(.:format)                    items#index
+  # POST             /items(.:format)                    items#create
 end
